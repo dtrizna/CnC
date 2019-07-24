d36af86afbfcd935fed9d91e3c33e3f95c926621
dtrizna <ditrizna@gmail.com>
2019-07-24 09:56:53 +0300

    README

--- server-agents.py
+++ server-agents.py
@@ -25,19 +25,6 @@ import inspect
 #  Client Thread - as now
 
 
-# TODO:
-#   - verification if agent is alive?
-#   [DONE] kill agent
-#   - list commands in agent mode!
-#   [DONE] correct exit from shell
-#   [DONE] help messages of Cmd 
-#   - add commands:
-#        getpid
-#        upload file
-#        migrate > inject itself
-#   - add HTTP transport
-#   - write C# agent
-
 def main():
     terminal = Terminal(Listeners,ClientDict)
     terminal.cmdloop()
