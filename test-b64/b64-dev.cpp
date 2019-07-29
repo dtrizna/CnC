06ab24929ed12cca2307855d3de042be472fb88a
dtrizna <ditrizna@gmail.com>
2019-07-29 15:19:53 +0300

    managed to get b64 working

--- test-b64/b64-dev.cpp
+++ test-b64/b64-dev.cpp
@@ -1,223 +1,170 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+//#include <inttypes.h>
+#include <string.h>
 
-/*
-** Translation Table as described in RFC1113
-*/
-static const char cb64[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-/*
-** Translation Table to decode (created by author)
-*/
-static const char cd64[]="|$$$}rstuvwxyz{$$$$$$$>?@ABCDEFGHIJKLMNOPQRSTUVW$$$$$$XYZ[\\]^_`abcdefghijklmnopq";
-
-/*
-** returnable errors
-**
-** Error codes returned to the operating system.
-**
-*/
-#define B64_SYNTAX_ERROR        1
-#define B64_FILE_ERROR          2
-#define B64_FILE_IO_ERROR       3
-#define B64_ERROR_OUT_CLOSE     4
-#define B64_LINE_SIZE_TO_MIN    5
-#define B64_SYNTAX_TOOMANYARGS  6
-
-/*
-** b64_message
-**
-** Gather text messages in one place.
-**
-*/
-#define B64_MAX_MESSAGES 7
-static char *b64_msgs[ B64_MAX_MESSAGES ] = {
-            "b64:000:Invalid Message Code.",
-            "b64:001:Syntax Error -- check help (-h) for usage.",
-            "b64:002:File Error Opening/Creating Files.",
-            "b64:003:File I/O Error -- Note: output file not removed.",
-            "b64:004:Error on output file close.",
-            "b64:005:linesize set to minimum.",
-            "b64:006:Syntax: Too many arguments."
-};
-
-#define b64_message( ec ) ((ec > 0 && ec < B64_MAX_MESSAGES ) ? b64_msgs[ ec ] : b64_msgs[ 0 ])
-
-/*
-** encodeblock
-**
-** encode 3 8-bit binary bytes as 4 '6-bit' characters
-*/
-static void encodeblock( unsigned char *in, unsigned char *out, int len )
+size_t b64_encoded_size(size_t inlen)
 {
-    out[0] = (unsigned char) cb64[ (int)(in[0] >> 2) ];
-    out[1] = (unsigned char) cb64[ (int)(((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4)) ];
-    out[2] = (unsigned char) (len > 1 ? cb64[ (int)(((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6)) ] : '=');
-    out[3] = (unsigned char) (len > 2 ? cb64[ (int)(in[2] & 0x3f) ] : '=');
+	size_t ret;
+
+	ret = inlen;
+	if (inlen % 3 != 0)
+		ret += 3 - (inlen % 3);
+	ret /= 3;
+	ret *= 4;
+
+	return ret;
 }
 
-/*
-** encode
-**
-** base64 encode a stream adding padding and line breaks as per spec.
-*/
-static int encode( FILE *infile, FILE *outfile, int linesize )
+const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+char *b64_encode(const unsigned char *in, size_t len)
 {
-    unsigned char in[3];
-    unsigned char out[4];
-    int i, len, blocksout = 0;
-    int retcode = 0;
+	char   *out;
+	size_t  elen;
+	size_t  i;
+	size_t  j;
+	size_t  v;
 
-    *in = (unsigned char) 0;
-    *out = (unsigned char) 0;
-    while( feof( infile ) == 0 ) {
-        len = 0;
-        for( i = 0; i < 3; i++ ) {
-            in[i] = (unsigned char) getc( infile );
+	if (in == NULL || len == 0)
+		return NULL;
 
-            if( feof( infile ) == 0 ) {
-                len++;
-            }
-            else {
-                in[i] = (unsigned char) 0;
-            }
-        }
-        if( len > 0 ) {
-            encodeblock( in, out, len );
-            for( i = 0; i < 4; i++ ) {
-                if( putc( (int)(out[i]), outfile ) == 0 ){
-                    if( ferror( outfile ) != 0 )      {
-                        perror( b64_message( B64_FILE_IO_ERROR ) );
-                        retcode = B64_FILE_IO_ERROR;
-                    }
-                    break;
-                }
-            }
-            blocksout++;
-        }
-        if( blocksout >= (linesize/4) || feof( infile ) != 0 ) {
-            if( blocksout > 0 ) {
-                fprintf( outfile, "\r\n" );
-            }
-            blocksout = 0;
-        }
-    }
-    return( retcode );
-}
+	elen = b64_encoded_size(len);
+	out  = (char *)malloc(elen+1);
+	out[elen] = '\0';
 
-/*
-** decodeblock
-**
-** decode 4 '6-bit' characters into 3 8-bit binary bytes
-*/
-static void decodeblock( unsigned char *in, unsigned char *out )
-{   
-    out[ 0 ] = (unsigned char ) (in[0] << 2 | in[1] >> 4);
-    out[ 1 ] = (unsigned char ) (in[1] << 4 | in[2] >> 2);
-    out[ 2 ] = (unsigned char ) (((in[2] << 6) & 0xc0) | in[3]);
-}
+	for (i=0, j=0; i<len; i+=3, j+=4) {
+		v = in[i];
+		v = i+1 < len ? v << 8 | in[i+1] : v << 8;
+		v = i+2 < len ? v << 8 | in[i+2] : v << 8;
 
-/*
-** decode
-**
-** decode a base64 encoded stream discarding padding, line breaks and noise
-*/
-static int decode( FILE *infile, FILE *outfile )
-{
-    int retcode = 0;
-    unsigned char in[4];
-    unsigned char out[3];
-    int v;
-    int i, len;
+		out[j]   = b64chars[(v >> 18) & 0x3F];
+		out[j+1] = b64chars[(v >> 12) & 0x3F];
+		if (i+1 < len) {
+			out[j+2] = b64chars[(v >> 6) & 0x3F];
+		} else {
+			out[j+2] = '=';
+		}
+		if (i+2 < len) {
+			out[j+3] = b64chars[v & 0x3F];
+		} else {
+			out[j+3] = '=';
+		}
+	}
 
-    *in = (unsigned char) 0;
-    *out = (unsigned char) 0;
-    while( feof( infile ) == 0 ) {
-        for( len = 0, i = 0; i < 4 && feof( infile ) == 0; i++ ) {
-            v = 0;
-            while( feof( infile ) == 0 && v == 0 ) {
-                v = getc( infile );
-                if( v != EOF ) {
-                    v = ((v < 43 || v > 122) ? 0 : (int) cd64[ v - 43 ]);
-                    if( v != 0 ) {
-                        v = ((v == (int)'$') ? 0 : v - 61);
-                    }
-                }
-            }
-            if( feof( infile ) == 0 ) {
-                len++;
-                if( v != 0 ) {
-                    in[ i ] = (unsigned char) (v - 1);
-                }
-            }
-            else {
-                in[i] = (unsigned char) 0;
-            }
-        }
-        if( len > 0 ) {
-            decodeblock( in, out );
-            for( i = 0; i < len - 1; i++ ) {
-                if( putc( (int) out[i], outfile ) == 0 ){
-                    if( ferror( outfile ) != 0 )      {
-                        perror( b64_message( B64_FILE_IO_ERROR ) );
-                        retcode = B64_FILE_IO_ERROR;
-                    }
-                    break;
-                }
-            }
-        }
-    }
-    return( retcode );
-}
-
-/*
-** b64
-**
-** 'engine' that opens streams and calls encode/decode
-*/
-
-static int b64( char opt, char *indata, char *outdata, int linesize )
-{
-    int retcode = 0;
-    //int retcode = B64_FILE_ERROR;
-    FILE *infile;
-    infile = fopen( indata, "rb" );
-    FILE *outfile;
-    outfile = fopen( outdata, "wb" );
-    // if there's file
-    if( !infile ) {
-        printf("[b64] [dbg] no such file");
-        infile = indata;   
-    }
-    else {
-        printf("[b64] [dbg] file exists");
-    }
-    
-    if (opt == 'e'){
-            retcode = encode(indata,);
-    }
-
-    return 0;
+	return out;
 }
 
 
-#define B64_DEF_LINE_SIZE   72
-#define B64_MIN_LINE_SIZE    4
-
-#define THIS_OPT(ac, av) ((char)(ac > 1 ? av[1][0] == '-' ? av[1][1] : 0 : 0))
-
-/*
-** main
-**
-** parse and validate arguments and call b64 engine or help
-*/
-int main( int argc, char **argv )
+size_t b64_decoded_size(const char *in)
 {
-    int retcode = 0;
-    int linesize = B64_DEF_LINE_SIZE;
-    char *infilename = NULL, *outfilename = NULL;
+	size_t len;
+	size_t ret;
+	size_t i;
 
-    b64('e',"test",NULL,linesize);
+	if (in == NULL)
+		return 0;
 
-    return( retcode );
+	len = strlen(in);
+	ret = len / 4 * 3;
+
+	for (i=len; i-->0; ) {
+		if (in[i] == '=') {
+			ret--;
+		} else {
+			break;
+		}
+	}
+
+	return ret;
+}
+
+int b64invs[80] { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
+	59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5,
+	6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28,
+	29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+	43, 44, 45, 46, 47, 48, 49, 50, 51 };
+
+// IF WANT TO GENERATE YOURSELF
+/* void b64_generate_decode_table()
+{
+	int    inv[80];
+	size_t i;
+
+	memset(inv, -1, sizeof(inv));
+	for (i=0; i<sizeof(b64chars)-1; i++) {
+		inv[b64chars[i]-43] = i;
+	}
+}*/
+
+int b64_isvalidchar(char c)
+{
+	if (c >= '0' && c <= '9')
+		return 1;
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	if (c >= 'a' && c <= 'z')
+		return 1;
+	if (c == '+' || c == '/' || c == '=')
+		return 1;
+	return 0;
+}
+
+char* b64_decode(const char *in)
+{
+	size_t len;
+	size_t i;
+	size_t j;
+	int    v;
+
+    size_t outlen;
+    outlen = b64_decoded_size(in);
+
+    char   *out;
+    out  = (char *)malloc(outlen+1);
+	out[outlen] = '\0';
+
+	if (in == NULL) {
+		return 0;
+    }
+	len = strlen(in);
+
+	if (outlen < b64_decoded_size(in) || len % 4 != 0) {
+		return 0;
+    }
+	for (i=0; i<len; i++) {
+		if (!b64_isvalidchar(in[i])) {
+			return 0;
+		}
+	}
+
+	for (i=0, j=0; i<len; i+=4, j+=3) {
+		v = b64invs[in[i]-43];
+		v = (v << 6) | b64invs[in[i+1]-43];
+		v = in[i+2]=='=' ? v << 6 : (v << 6) | b64invs[in[i+2]-43];
+		v = in[i+3]=='=' ? v << 6 : (v << 6) | b64invs[in[i+3]-43];
+
+		out[j] = (v >> 16) & 0xFF;
+		if (in[i+2] != '=')
+			out[j+1] = (v >> 8) & 0xFF;
+		if (in[i+3] != '=')
+			out[j+2] = v & 0xFF;
+	}
+	return out;
+}
+
+int main() {
+    // ENCODING WORKS AS:
+    const unsigned char* test = reinterpret_cast<const unsigned char *>( "test" );
+    char * out;
+    out =  b64_encode(test,sizeof(test));
+    printf("%s if encoded as Base64 is: %s\n",test,out);
+
+    // DECODING WORKS AS:
+    char * test2 = "dGVzdGluZ3NvbWVsb25nc3R5bGUK";
+    char * out2;
+    out2 = b64_decode(test2);
+    printf("%s if decoded as Base64 is: %s\n", test2, out2);
 }
